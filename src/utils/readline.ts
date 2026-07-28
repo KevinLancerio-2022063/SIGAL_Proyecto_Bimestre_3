437987b439f303739864793b095e627f1585e655
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-07-28 11:39:25 -0600

    feat: Se ha subido el readline

--- src/utils/readline.ts
+++ src/utils/readline.ts
@@ -1,6 +1,13 @@
-import * as readline from 'readline';
+import * as readline from 'readline/promises';
+import { stdin as input, stdout as output } from 'process';
 
-export const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
+// Creamos la interfaz usando promesas para poder usar async/await
+export const rl = readline.createInterface({ input, output });
+
+// Función auxiliar para hacer preguntas de forma asíncrona
+export const ask = async (question: string): Promise<string> => {
+    return await rl.question(question);
+};
+
+// Función para cerrar la interfaz al salir
+export const closeRl = () => rl.close();
